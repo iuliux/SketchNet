4811859c140118e5d56b514d0f4d78a0c6671c20
Iulius Curt <iulius.curt@gmail.com>
2013-09-09 19:47:16 +0300

    Added rotation. Encapsulated in a function.

--- svgutils.test.py
+++ svgutils.test.py
@@ -1,45 +1,67 @@
 import svgutils.transform as sg
 import math
 
-#create new SVG figure
-fig = sg.SVGFigure("1500px", "1500px")
+
+def line_angle(line):
+    p1, p2 = line
+    xDiff = p2[0] - p1[0]
+    yDiff= p2[1] - p1[1]
+
+    return math.degrees(math.atan2(yDiff, xDiff))
+
+
+def register_sketches(sk1, sk2, mp1=1, mp2=1):
+    def parse_line(elem):
+        return ((float(elem.get('x1')), float(elem.get('y1'))),
+                (float(elem.get('x2')), float(elem.get('y2'))))
+
+    up1 = parse_line(sk1.find_id('MP_' + str(mp1) + '_u').root)
+    rg1 = parse_line(sk1.find_id('MP_' + str(mp1) + '_r').root)
+    up2 = parse_line(sk2.find_id('MP_' + str(mp2) + '_u').root)
+    rg2 = parse_line(sk2.find_id('MP_' + str(mp2) + '_r').root)
+
+    print '--angle-->', line_angle(up2)
+
+    fig1scale = math.sqrt((up1[0][0] - up1[1][0])**2 + (up1[0][1] - up1[1][1])**2)
+    fig2scale = math.sqrt((up2[0][0] - up2[1][0])**2 + (up2[0][1] - up2[1][1])**2)
+    print '--scales-->', fig1scale, fig2scale
+    midscale = (fig1scale + fig2scale) / 2.
+    fig1_normscale = fig1scale / midscale
+    fig2_normscale = fig2scale / midscale
+    print '--normscales-->', fig1_normscale, fig2_normscale
+
+    displacement = (up1[0][0]*fig2_normscale - up2[0][0]*fig1_normscale,
+                    up1[0][1]*fig2_normscale - up2[0][1]*fig1_normscale)
+
+    # get the plot objects
+    plot1 = fig1.getroot()
+    plot2 = fig2.getroot()
+    plot1.moveto(500, 500, scale=fig2_normscale)
+    plot2.moveto(500 + displacement[0], 500 + displacement[1], scale=fig1_normscale)
+
+    # TODO: Rotation
+    plot1.rotate(-90 - line_angle(up1))
+    plot2.rotate(-90 - line_angle(up2))
+
+    # add text labels
+    txt1 = sg.TextElement(25,20, "A", size=32, weight="bold")
+    txt2 = sg.TextElement(305,20, "B", size=32, weight="bold")
+
+    #create new SVG figure
+    fig = sg.SVGFigure("1500px", "1500px")
+
+    # append plots and labels to figure
+    fig.append([plot1, plot2])
+    fig.append([txt1, txt2])
+
+    return fig
+
 
 # load matpotlib-generated figures
 fig1 = sg.fromfile('mpt1.svg')
 fig2 = sg.fromfile('mpt2.svg')
 
-up1 = fig1.find_id('MP_1_u').root
-print up1
-rg1 = fig1.find_id('MP_1_r').root
-up2 = fig2.find_id('MP_1_u').root
-rg2 = fig2.find_id('MP_1_r').root
-
-fig1scale = math.sqrt((float(up1.get('x1')) - float(up1.get('x2')))**2 + (float(up1.get('y1')) - float(up1.get('y2')))**2)
-fig2scale = math.sqrt((float(up2.get('x1')) - float(up2.get('x2')))**2 + (float(up2.get('y1')) - float(up2.get('y2')))**2)
-print fig1scale, fig2scale
-midscale = (fig1scale + fig2scale) / 2.
-fig1_normscale = fig1scale / midscale
-fig2_normscale = fig2scale / midscale
-print fig1_normscale, fig2_normscale
-
-displacement = (float(up1.get('x1'))*fig2_normscale - float(up2.get('x1'))*fig1_normscale, float(up1.get('y1'))*fig2_normscale - float(up2.get('y1'))*fig1_normscale)
-
-# get the plot objects
-plot1 = fig1.getroot()
-plot2 = fig2.getroot()
-plot1.moveto(500, 500, scale=fig2_normscale)
-plot2.moveto(500 + displacement[0], 500 + displacement[1], scale=fig1_normscale)
-
-# TODO: Rotation
-# plot2.rotate(45)
-
-# add text labels
-txt1 = sg.TextElement(25,20, "A", size=32, weight="bold")
-txt2 = sg.TextElement(305,20, "B", size=32, weight="bold")
-
-# append plots and labels to figure
-fig.append([plot1, plot2])
-fig.append([txt1, txt2])
-
 # save generated SVG files
-fig.save("fig_final.svg")
+regfig = register_sketches(fig1, fig2)
+
+regfig.save("fig_final.svg")
