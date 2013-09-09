c37912026f83d1678492dae1271be7b0ddea3823
Iulius Curt <iulius.curt@gmail.com>
2013-09-10 00:05:46 +0300

    Fixed rotation in register_sketches.

--- svgutils.test.py
+++ svgutils.test.py
@@ -15,44 +15,42 @@ def register_sketches(sk1, sk2, mp1=1, mp2=1):
         return ((float(elem.get('x1')), float(elem.get('y1'))),
                 (float(elem.get('x2')), float(elem.get('y2'))))
 
+    # Get mounting points guide lines
     up1 = parse_line(sk1.find_id('MP_' + str(mp1) + '_u').root)
     rg1 = parse_line(sk1.find_id('MP_' + str(mp1) + '_r').root)
     up2 = parse_line(sk2.find_id('MP_' + str(mp2) + '_u').root)
     rg2 = parse_line(sk2.find_id('MP_' + str(mp2) + '_r').root)
 
-    print '--angle-->', line_angle(up2)
-
+    # Compute scales to make the two figures match in size
     fig1scale = math.sqrt((up1[0][0] - up1[1][0])**2 + (up1[0][1] - up1[1][1])**2)
     fig2scale = math.sqrt((up2[0][0] - up2[1][0])**2 + (up2[0][1] - up2[1][1])**2)
-    print '--scales-->', fig1scale, fig2scale
-    midscale = (fig1scale + fig2scale) / 2.
+    midscale = (fig1scale + fig2scale) / 2.  # The average of the two scales
     fig1_normscale = fig1scale / midscale
     fig2_normscale = fig2scale / midscale
+    print '--scales-->', fig1scale, fig2scale
     print '--normscales-->', fig1_normscale, fig2_normscale
 
+    # 2D displacement of the second figure
     displacement = (up1[0][0]*fig2_normscale - up2[0][0]*fig1_normscale,
                     up1[0][1]*fig2_normscale - up2[0][1]*fig1_normscale)
 
-    # get the plot objects
+    # Get the plot objects
     plot1 = fig1.getroot()
     plot2 = fig2.getroot()
-    plot1.moveto(500, 500, scale=fig2_normscale)
-    plot2.moveto(500 + displacement[0], 500 + displacement[1], scale=fig1_normscale)
+    plot1.moveto(0, 0, scale=fig2_normscale)
+    plot2.moveto(0 + displacement[0], 0 + displacement[1], scale=fig1_normscale)
 
-    # TODO: Rotation
-    plot1.rotate(-90 - line_angle(up1))
-    plot2.rotate(-90 - line_angle(up2))
+    # Rotation
+    plot1.rotate(-90 - line_angle(up1), up1[0][0], up1[0][1])
+    plot2.rotate(-90 - line_angle(up2), up2[0][0], up2[0][1])
 
-    # add text labels
-    txt1 = sg.TextElement(25,20, "A", size=32, weight="bold")
-    txt2 = sg.TextElement(305,20, "B", size=32, weight="bold")
+    # TODO: Flip if needed
 
-    #create new SVG figure
+    # Create new SVG figure
     fig = sg.SVGFigure("1500px", "1500px")
 
-    # append plots and labels to figure
+    # Append plots and labels to figure
     fig.append([plot1, plot2])
-    fig.append([txt1, txt2])
 
     return fig
 
